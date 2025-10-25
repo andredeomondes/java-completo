19007a8369f03e5333df3aef2f04fa90b8838624
andredeomondes <andredeomondes.dev@gmail.com>
2025-10-25 18:40:42 -0300

    Completando a seção 17 capitulo 179 tipo generics

---
+++ s17c179_introducao_generics/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import services.PrintService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Value #" + (i + 1) + ": ");
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        Integer x = ps.first();
+        System.out.println("First: " + x);
+
+        sc.close();
+    }
+}
